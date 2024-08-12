f655725febcaabd1978dbf3a0c97bdf0132160b1
finderfail <shadowmaster1957@outlook.com>
2024-08-12 17:54:56 +0300

    releae 0.0.1

--- index.js
+++ index.js
@@ -1,45 +1,85 @@
 const os = require('node:os');
+// import request from 'request';
+const request = require('request');
+require('dotenv').config()
 
 let arch = os.arch()
 let mem = os.totalmem()
 let hostname = os.hostname()
 let platform = os.platform()
+// chalk.level = 1;
 // console.log(arch, cpu, hostname);
 
 async function main() {
     //yeah yeah functions in js dont hate.
     console.log("Hello", hostname);
-    console.log("Welcome to Abstract Compability Check programm");
-    console.log("Its been rewriting 3 times!");
+    console.log(("Welcome to Abstract Compability Check programm"));
+    // console.log("Its been rewriting 3 times!");
     await delay(1000);
     console.log("Please wait programm will be check your pc");
     await delay(2500);
     if (arch == "x64") {
-        console.log("You have need cpu arch:", arch);
+        console.log("You have need cpu arch:", (arch));
         await delay(1500);
         oscheck()
     } else if (arch == "arm64") {
-        console.log("You have need cpu arch:", arch);
+        console.log("You have need cpu arch:", (arch));
         await delay(500);
         oscheck()
     } else {
-        console.log("Your cpu arch is deprecated", arch, "or Abstract not created mod version for you!");
+        console.log("Your cpu arch is deprecated", (arch), "or Abstract not created mod version for you!");
     }   
 }
 
-function oscheck() {
-    if (platform == "win32") {
-        console.log("Your system is compatible:", platform);
-    } else if (platform == "darwin") {
-        console.log("Your system is partially compatible:", platform);
+async function oscheck() {
+    if (platform == "win32") { //32 is not cpu bits
+        console.log("Your system is compatible:", (platform));
+        await delay(1500);
+        memcheck()
+    } else if (platform == "darwin") { //yeah darwin support in Abstract
+        console.log("Your system is partially compatible:", (platform));
+        await delay(500);
+        memcheck()
     } else {
-        console.log("Your system is not compatible:", platform);
+        console.log("Your system is not compatible:", (platform));
     }
+}
+
+function memcheck() {
+   // console.log(mem);
+    if (mem >= 4000000000) {
+        let memO = mem/1024/1024/1024;
+        console.log("You have enough RAM:", (memO), "GB");
+        work_abs() 
+    } else {
+        console.log(("You dont have enough ram memory"))
+    }
+}
+
+async function work_abs() {
+    await delay(2000);
+    if (platform == "win32") {
+        request(process.env.WIN32,
+            (err, response, body) => {
+                return console.log(body);
+            }
+        );
+    } else if (platform == "darwin") {
+        request(process.env.DARWIN,
+            (err, response, body) => {
+                return console.log(body);
+            }
+        );
+    } else {
+        console.log(("Why are you here?"));
+    }
+
 }
 
 
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
+    // the most easy and based delay btw
 } 
 
 
