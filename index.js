30a1b97c6aa0e9413e146aae2b62ec23d193ea29
finderfail <ilyalvov2007@gmail.com>
2024-08-17 00:09:55 +0300

    speed up (xd)

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const os = require('node:os');
-// import request from 'request';
 const request = require('request');
 require('dotenv').config()
 
@@ -7,24 +6,19 @@ let arch = os.arch()
 let mem = os.totalmem()
 let hostname = os.hostname()
 let platform = os.platform()
-// chalk.level = 1;
-// console.log(arch, cpu, hostname);
 
 async function main() {
     //yeah yeah functions in js dont hate.
     console.log("Hello", hostname);
     console.log(("Welcome to Abstract Compability Check programm"));
-    // console.log("Its been rewriting 3 times!");
-    await delay(1000);
     console.log("Please wait programm will be check your pc");
     await delay(2500);
     if (arch == "x64") {
         console.log("You have need cpu arch:", (arch));
-        await delay(1500);
+        await delay(500);
         oscheck()
     } else if (arch == "arm64") {
         console.log("You have need cpu arch:", (arch));
-        await delay(500);
         oscheck()
     } else {
         console.log("Your cpu arch is deprecated", (arch), "or Abstract not created mod version for you!");
@@ -34,11 +28,11 @@ async function main() {
 async function oscheck() {
     if (platform == "win32") { //32 is not cpu bits
         console.log("Your system is compatible:", (platform));
-        await delay(1500);
+        await delay(500);
         memcheck()
     } else if (platform == "darwin") { //yeah darwin support in Abstract
         console.log("Your system is partially compatible:", (platform));
-        await delay(500);
+        await delay(190);
         memcheck()
     } else {
         console.log("Your system is not compatible:", (platform));
